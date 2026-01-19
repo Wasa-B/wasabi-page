d63f431233faa9398ddf5902a88de76fd38f3e4e
Wasa-B <ghd1876@gmail.com>
2026-01-19 16:48:13 +0900

    feat: fix skill icon

--- src/components/common/Iconbox.tsx
+++ src/components/common/Iconbox.tsx
@@ -2,7 +2,10 @@ const Iconbox = ({ icon }: { icon: string }) => {
   return (
     <div>
       <div className="w-23 h-23 flex rounded-lg items-center justify-center bg-[var(--icon-bg)] ">
-        <div className="w-20 h-20 rounded-lg bg-slate-50 flex items-center justify-center">
+        <div
+          className="w-20 h-20 rounded-lg bg-[var(--icon-inner-bg)] flex items-center justify-center"
+          style={{ boxShadow: 'var(--icon-shadow)' }}
+        >
           <div className="w-15 h-15">
             <img src={`icons/${icon}.svg`} alt={`${icon} icon`} />
           </div>
